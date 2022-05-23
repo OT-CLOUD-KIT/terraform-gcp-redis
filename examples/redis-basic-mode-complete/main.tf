f7c038c71683d24e663bc45d4349a0b223ea9735
Pawan Chandna <pawan.chandna@opstree.com>
2022-05-23 21:47:36 +0530

    change module source from "../" to "OT-CLOUD-KIT/redis/gcp" into readme file

--- examples/redis-basic-mode-complete/main.tf
+++ examples/redis-basic-mode-complete/main.tf
@@ -1,5 +1,5 @@
 module "redis" {
-  source                  = "../../"
+  source                  = "OT-CLOUD-KIT/redis/gcp"
   vpc_network             = "squadup-vpc"
   primary_zone_id         = "us-east1-b"
   use_private_g_services  = true
